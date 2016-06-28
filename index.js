c1c0eb7d1f83dfd15a7dac3e63a84a9b1153f623
Keith Showalter <kshowalter@fsec.ucf.edu>
2016-06-28 16:56:30 -0400

    first attemts at an API

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
+var helper = {
+  type: 'specDOM_helper',
 
+
+  div: function(){
+    var h = Object.create(helper);
+    h.config = {
+      tag: 'div',
+      props: {},
+      children: [
+        
+      ]
+    };
+
+
+    return h;
+  }
+
+};
 
 /**
  * .
  * @exports
  */
-module.exports = {
+module.exports = function(){
+  var h = Object.create(helper);
+  h.config = {};
 
-
+  return h;
 };
